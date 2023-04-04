7773e2db7fc93e1492f5170df312f18988708508
deschatr <nicolas@lappis.com>
2023-04-04 12:42:56 +0100

    FinePaymentService
    Co-authored-by: Paul Lynch <PaulLynch123@users.noreply.github.com>
    Co-authored-by: SeanMc913 <SeanMc913@users.noreply.github.com>

---
+++ Data/IFinePaymentService.cs
@@ -0,0 +1,11 @@
+using FinePaymentApp.Data;
+
+public interface IFinePaymentService
+{
+    Task<IEnumerable<FinePayment>> GetFinePayments();
+    Task<FinePayment?> GetFinePayment(long id);
+    Task<FinePayment?> SearchFinePayment(string? caseRef, string? onlineAccountRef);
+    Task<bool> AddFinePayment(FinePayment finePayment);
+    Task<bool> DeleteFinePayment(FinePayment finePayment);
+    Task<bool> SetFinePayment( FinePayment finePayment);
+}
