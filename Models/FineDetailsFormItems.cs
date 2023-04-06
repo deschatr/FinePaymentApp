6b4b89ce4df4c17fb5adfd0736f869fa974545d9
deschatr <nicolas@lappis.com>
2023-04-06 12:22:16 +0100

    reverted changes

--- Models/FineDetailsFormItems.cs
+++ Models/FineDetailsFormItems.cs
@@ -3,10 +3,10 @@ using System.ComponentModel.DataAnnotations;
 public class FineDetailsFormItems
 {
     [Required]
-    [StringLength(9, MinimumLength = 9, ErrorMessage = "The format of the case reference is 23/123456")]
+    [StringLength(6, MinimumLength = 6, ErrorMessage = "9 characters please")]
     public string CaseReference { get; set; } = "";
     
     [Required]
-    [StringLength(12, MinimumLength = 12, ErrorMessage = "The online account reference should be 12 characters: NIDF12345678")]
+    [MinLength(6)]
     public string OnlineAccountReference { get; set; } = "";
 }
