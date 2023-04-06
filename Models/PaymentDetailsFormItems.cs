ae279884b0e146c4c04c98cb4333bce2dbf865c5
deschatr <nicolas@lappis.com>
2023-04-06 12:41:29 +0100

    more changes to commit again

--- Models/PaymentDetailsFormItems.cs
+++ Models/PaymentDetailsFormItems.cs
@@ -6,13 +6,18 @@ public class PaymentDetailsFormItems
     public string? CardType { get; set; }
 
     [Required]
-    [StringLength(maximumLength: 16, MinimumLength = 16, ErrorMessage = "9 characters please")]
+    [CreditCard]
     public string? CardNumber{ get; set; }
 
     public DateOnly ExpiryDate { get; set; }
+    [Required]
+    public string? ExpiryMonth { get; set; }
+    [Required]
+    public string? ExpiryYear { get; set; }
+
 
     [Required]
-    [StringLength(maximumLength: 3, MinimumLength = 3, ErrorMessage = "9 characters please")]
+    [StringLength(maximumLength: 3, MinimumLength = 3, ErrorMessage = "3 characters please")]
     public string? Csv{ get; set; }
     // etc...
 
